c48cf5b34d441254519381c00b374570930da656
quoctrang12 <waipayttt@gmail.com>
2022-09-14 09:25:08 +0700

    Xay dung trang tong quan cac san pham

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/products_manager.dart';
+import 'ui/products/product_overview_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -22,8 +23,11 @@ class MyApp extends StatelessWidget {
           secondary: Colors.deepOrange,
         ),
       ),
-      home: SafeArea(
-        child: ProductDetailScreen(ProductsManager().items[0],),
+      // home: SafeArea(
+      //   child: ProductDetailScreen(ProductsManager().items[0],),
+      // ),
+      home: const SafeArea(
+        child: ProductsOverviewScreen(),
       ),
     );
   }
